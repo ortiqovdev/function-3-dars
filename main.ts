cf7077c50a2b8088c73440fccd781050d74d45ef
ortiqovdev <akmalbekortiqov7976@gmail.com>
2023-02-02 11:44:01 +0500

    function 3 dars

---
+++ main.ts
@@ -0,0 +1,67 @@
+//  * function
+// * 02.02.2023 10:00
+
+// ! Birinchi masala
+// function millisecund(){
+// }
+// const anonim: () => void = function(){
+// }
+// type Func = (a:number, b: number)=> string
+// const plus: Func = function a(a:number,b :number){
+//     return ""
+//  }
+//  const minus: Func = (a,b)=>{
+//     return ""
+// } 
+// ! Ikkinchi masala
+// type Op = ( a:number , b: number)=> number
+// function plus(a:number, b: number):number{
+//     let c: number = a+b
+//     return c
+// }
+// function minus(a:number, b: number):number{
+//     let c: number = a-b
+//     return c
+// }
+// function division(a:number, b: number):number{
+//     let c: number = a/b
+//     return c
+// }
+// function multiply(a:number, b: number):number{
+//     let c: number = a*b
+//     return c
+// }
+
+// const operation : Op = minus
+// const result: number = operation(10,5)
+// console.log(result);
+// const c: string = ''
+
+// type ops = '+'|'-'|'/'| '*'
+// const calculator = (a:number, b: number, c:ops) => {
+// let operation: Op = multiply
+// switch(c){
+//     case '+': operation = plus; break
+//     case '-': operation = minus; break
+//     case '/': operation = division; break
+//     case '*': operation = multiply; break
+// }
+//    return operation(a,b)
+// }
+// const res: number= calculator(10 ,5 , '-')
+// console.log(res);
+
+// ! Uchinchi masala 1-usul
+const massiv: number[] = [1, 2, 3, 4, 5]
+const n: number | undefined = massiv.find(v => v > 3 && v < 5)
+console.log(n);
+// ! Uchinchi masala 2-usul
+function filter(massiv: number[]): number | undefined {
+    for (let v of massiv) {
+        if (v > 1 && v < 5) {
+            return v
+        }
+    }
+}
+let c:number | undefined =filter([3,2,4,5,1,12])
+console.log(c);
