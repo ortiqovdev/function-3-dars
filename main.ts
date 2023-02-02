203231a80b121b57fc2081a74ab3850fcbc6ad0a
Akmal Ortiqov <116548309+ortiqovdev@users.noreply.github.com>
2023-02-02 12:41:06 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -52,16 +52,39 @@
 // console.log(res);
 
 // ! Uchinchi masala 1-usul
-const massiv: number[] = [1, 2, 3, 4, 5]
+// const massiv: number[] = [1, 2, 3, 4, 5]
+// const n: number | undefined = massiv.find(v => v > 3 && v < 5)
+// console.log(n);
+// // ! Uchinchi masala 2-usul
+
+const massiv: number[] = [1, 2, 3, 4, 5, 6]
+
 const n: number | undefined = massiv.find(v => v > 3 && v < 5)
-console.log(n);
-// ! Uchinchi masala 2-usul
-function filter(massiv: number[]): number | undefined {
-    for (let v of massiv) {
-        if (v > 1 && v < 5) {
+
+console.log(n)
+
+// -----------------------------------
+type Condition = (v: number) => boolean
+
+function find(massiv: number[], condition: Condition): number | undefined {
+
+    for(let v of massiv) {
+        if (condition(v)) {
             return v
         }
     }
 }
-let c:number | undefined =filter([3,2,4,5,1,12])
-console.log(c);
+
+let c: number | undefined = find([3, 2, 4, 5, 1, 12], (v: number) => {
+    return v >= 1 && v <= 5 
+})
+
+let ccc: number[] = [3, 2, 4, 5, 1, 12]
+
+let c1 = find(ccc, (v: number) => v % 2 == 0 && v > 3)
+
+ccc.find(v => v % 2 == 0 && v > 3)
+
+console.log(c)
+console.log(c1)
+
